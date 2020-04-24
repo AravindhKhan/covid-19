bf2b934807947d48887f5a17974596183fef4ec5
AravindhS <aravindh.s2@cognizant.com>
2020-04-24 10:32:21 +0530

    Third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TreeGridModule, SortService, FilterService } from '@syncfusion/ej2-angular-treegrid';
 import { HeaderComponent } from './components/header/header.component';
 import { TrackerComponent } from './components/tracker/tracker.component';
 import { ChartModule, DateTimeService, LineSeriesService, DateTimeCategoryService, StripLineService } from '@syncfusion/ej2-angular-charts';
-import { MapsModule } from '@syncfusion/ej2-angular-maps';
+import { ChartComponent } from './components/chart/chart.component';
+import { HelplineComponent } from './components/helpline/helpline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TrackerComponent
+    TrackerComponent,
+    ChartComponent,
+    HelplineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     TreeGridModule,
     ChartModule,
-    MapsModule
+    HttpClientModule
   ],
   providers: [
     SortService,
--- src/app/components/tracker/tracker.component.ts
+++ src/app/components/tracker/tracker.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SortSettingsModel } from '@syncfusion/ej2-angular-treegrid';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { TrackerService } from '../service/tracker.service';
 
 @Component({
   selector: 'app-tracker',
@@ -9,56 +11,60 @@ import { Observable } from 'rxjs';
 })
 export class TrackerComponent implements OnInit {
 
-  data:Object[]=[
-    {
-      "states": "Tamil Nadu",
-      "confirmed": 1520,
-      "active": 1046,
-      "recovered":457,
-      "deceased":17,
-      districts:[
-        {
-          "states": "Chennai",
-          "confirmed": 500,
-          "active": 499,
-          "recovered":499,
-          "deceased":0
-        },
-        {
-          "states": "Tiruppur",
-          "confirmed": 320,
-          "active": 310,
-          "recovered":10 ,
-          "deceased":0
-        }
-      ]
-    },
-    {
-      "states": "Maharastra",
-      "confirmed": 4666,
-      "active": 3862,
-      "recovered":572,
-      "deceased":232,
-      districts:[
-        {
-          "states": "Mumbai",
-          "confirmed": 2455,
-          "active": 2450,
-          "recovered":5,
-          "deceased":0
-        },
-        {
-          "states": "Pune",
-          "confirmed": 591,
-          "active": 590,
-          "recovered":1,
-          "deceased":0
-        }
-      ]
-    }
+  data:Object[] =[
+    // {
+    //   "states": "Tamil Nadu",
+    //   "confirmed": 1520,
+    //   "active": 1046,
+    //   "recovered":457,
+    //   "deceased":17,
+    //   subtasks:[
+    //     {
+    //       "states": "Chennai",
+    //       "confirmed": 500,
+    //       "active": 499,
+    //       "recovered":499,
+    //       "deceased":0
+    //     },
+    //     {
+    //       "states": "Tiruppur",
+    //       "confirmed": 320,
+    //       "active": 310,
+    //       "recovered":10 ,
+    //       "deceased":0
+    //     }
+    //   ]
+    // },
+    // {
+    //   "states": "Maharastra",
+    //   "confirmed": 4666,
+    //   "active": 3862,
+    //   "recovered":572,
+    //   "deceased":232,
+    //   subtasks:[
+    //     {
+    //       "states": "Mumbai",
+    //       "confirmed": 2455,
+    //       "active": 2450,
+    //       "recovered":5,
+    //       "deceased":0
+    //     },
+    //     {
+    //       "states": "Pune",
+    //       "confirmed": 591,
+    //       "active": 590,
+    //       "recovered":1,
+    //       "deceased":0
+    //     }
+    //   ]
+    // }
   ];
-
+  
   public sortSetting:SortSettingsModel;
+
+  constructor(private trackerService: TrackerService){
+
+  }
 
   ngOnInit(){
     this.sortSetting = {
@@ -77,8 +83,27 @@ export class TrackerComponent implements OnInit {
         }
       ]
     }
-    setTimeout(() => {
-      console.log("reloaded");
-    }, 1000);
+    this.trackerService.getTrackerdata().subscribe(
+    datas => {
+      var newArray = [];
+      console.log('datas',datas);
+       newArray.push.apply(newArray, datas);
+       console.log('array',newArray)
+       this.data = newArray
+       console.log("in get request", this.data);
+    }
+     );
+    //  setInterval(function(){ 
+    //   this.trackerService.getTrackerdata().subscribe(
+    //     datas => {
+    //       var newArray = [];
+    //       console.log('datas',datas);
+    //        newArray.push.apply(newArray, datas);
+    //        console.log('array',newArray)
+    //        this.data = newArray
+    //        console.log("in get request", this.data);
+    //     }
+    //      );
+    //   }, 3000);
   }
 }
